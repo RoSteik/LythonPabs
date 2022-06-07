ea41e5a03e6005b3bdc593a1bb289a1a1281e251
RoSteik <rostyslav.postolaki@gmail.com>
2022-06-07 20:28:05 +0300

    converted code in function2

---
+++ laba9/lab11/__init__.py
@@ -0,0 +1,7 @@
+""" 
+Created by RoSteik (Telegram: @Rosteik)
+"""
+from regex import extract_info_from_zipfile
+
+if __name__ == '__main__':
+    extract_info_from_zipfile()
--- laba9/lab11/regex.py
+++ laba9/lab11/regex.py
@@ -5,19 +5,22 @@ Created by RoSteik (Telegram: @Rosteik)
 import re as regex
 import zipfile
 
-if __name__ == '__main__':
+
+def extract_info_from_zipfile():
     lines_list_with_regex = list()
 
-    file_zip = zipfile.ZipFile('server.log (2).zip', 'r')
+    zipfile_name = 'server.log (2).zip'
+    file_zip = zipfile.ZipFile(zipfile_name, 'r')
     file_zip.extractall('./')
     file_zip.close()
 
-    with open('server.log (2).2018-04-04') as file:
+    file_name_from_zip = 'server.log (2).2018-04-04'
+
+    with open(file_name_from_zip) as file:
         lines_list_from_file = [line.rstrip() for line in file.readlines()]
 
     for line in lines_list_from_file:
-        line_to_find = regex.findall(r"INFO.*WFLY.*", line)
-
+        line_to_find = regex.findall(r"INFO\s+\[.*wildfly.*].*WFLY.*", line)
         if line_to_find:
             lines_list_with_regex.append(line_to_find)
 
