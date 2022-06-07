ec074fe5c59f9d08c8621a3d763a65ec693690c9
RoSteik <rostyslav.postolaki@gmail.com>
2022-06-07 19:19:54 +0300

    added code which unzips file_zip

--- laba9/lab11/main.py
+++ laba9/lab11/main.py
@@ -1,6 +1,7 @@
 """ 
 Created by RoSteik (Telegram: @Rosteik)
 """
+
 import re as regex
 import zipfile
 
@@ -15,7 +16,7 @@ if __name__ == '__main__':
         lines_list_from_file = [line.rstrip() for line in file.readlines()]
 
     for line in lines_list_from_file:
-        line_to_find = regex.findall("INFO.*WFLY.*", line)
+        line_to_find = regex.findall(r"INFO.*WFLY.*", line)
 
         if line_to_find:
             lines_list_with_regex.append(line_to_find)
